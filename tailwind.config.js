7b892e54fec04509cc543514b2314ea71ad9ae14
Chen Zhiqiang <1921437884@qq.com>
2023-02-01 16:39:32 +0800

    feat: 添加点阵行进动画

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,12 @@ module.exports = {
           "linear-gradient(268.86deg, #120720 -47%, rgba(9, 196, 255, 0.9) -7.98%, #2456A2 39.83%, #5E388D 89.26%, #3D235F 123.82%, #000000 151.73%)",
         "gradient-btn-outline":
           "linear-gradient(to right, #fff, #fff), linear-gradient(var(--rotate), #120720 0.99%, rgba(9, 196, 255, 0.9) 20.51%, #2456A2 44.43%, #B073FF 64.36%, #3D235F 85.15%, #000 100.41%)",
+        // 亮色圆点背景
+        "dot-light":
+          "radial-gradient(rgba(0, 0, 0, 0.3) 20%, transparent 0.5rem)",
+        // 暗色圆点背景
+        "dot-dark":
+          "radial-gradient(rgba(255, 255, 255, 0.3) 20%, transparent 0.5rem)",
       },
       borderWidth: {
         3: "3px",
@@ -30,21 +36,21 @@ module.exports = {
         noto: ["Noto Sans"],
       },
       keyframes: {
-        "bg-position": {
+        "bg-translateX": {
           "0%,50%": { "background-position": "150% 0" },
           "75%": { "background-position": "75% 0" },
           "100%": { "background-position": "0 0" },
         },
-        "bg-rotate": {
+        rotate: {
           "0%": { "--rotate": "268deg" },
           "100%": { "--rotate": "-92deg" },
         },
       },
       animation: {
         // 背景流动动画
-        "bg-flow": "bg-position 3s linear infinite",
+        "bg-translateX": "bg-translateX 3s linear infinite",
         // 背景旋转动画
-        "bg-rotate": "bg-rotate 4s linear infinite",
+        "bg-rotate": "rotate 4s linear infinite",
       },
     },
   },
